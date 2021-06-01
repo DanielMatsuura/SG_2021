34c40eda04fa6b646b3a039c55d3ddefd1f1244d
DanielMatsuura <85190874+DanielMatsuura@users.noreply.github.com>
2021-06-01 14:13:46 -0400

    Primera version de los modelos

---
+++ Models/IdentityModels.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Prueba2.Models
+{
+    // Para agregar datos de perfil del usuario, agregue más propiedades a su clase ApplicationUser. Visite https://go.microsoft.com/fwlink/?LinkID=317594 para obtener más información.
+    public class ApplicationUser : IdentityUser
+    {
+
+        public string FullName { get; set; }
+
+        //public int Age { get; set; }
+
+
+
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Tenga en cuenta que el valor de authenticationType debe coincidir con el definido en CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Agregar aquí notificaciones personalizadas de usuario
+            return userIdentity;
+        }
+    }
+
+    
+}
